8d8681e524cd1f6f11b8abf9ec67024424480795
Tyler Moses <AF17783@LC02ND21LG3QN>
2017-03-16 19:17:01 -0400

    _bower updates angular and jquery dynamically

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,7 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = Generator.extend({
+  // Interact With User
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
@@ -18,20 +19,23 @@ module.exports = Generator.extend({
         default: this.appname
       },
       {
-        type: 'rawList',
+        type: 'list',
         name: 'angular',
         message: 'What Version of Angular would you like to use?',
         choices: ['1.6', '2.0'],
         defualt: '1.6'
       },
       {
-        type: 'confirm',
+        type: 'list',
         name: 'jquery',
-        message: 'Would you like to include jQuery?',
-        default: true
+        message: 'jQuery?',
+        choices: ['lite', '1.x', '2.x', '3.x'],
+        default: 'lite'
       }
     ];
+
     var done = this.async();
+
     this.prompt(prompts)
     .then(function (answers) {
       this.props = answers;
@@ -40,6 +44,7 @@ module.exports = Generator.extend({
       done();
     }.bind(this));
   },
+  // Write Files
   writing: {
     // Copy Configuration files
     config: function () {
@@ -54,8 +59,7 @@ module.exports = Generator.extend({
       this.fs.copyTpl(
         this.templatePath('_bower.json'),
         this.destinationPath('bower.json'), {
-          name: this.props.name,
-          angular: this.props.angular
+          name: this.props.name
         }
       );
       this.fs.copy(
@@ -64,10 +68,13 @@ module.exports = Generator.extend({
       );
     }
   },
+  // Install Dependencies
   install: function () {
+    this.conflicter.force = true;
     this.installDependencies();
-    if (this.props.jquery === true) {
-      this.bowerInstall(['jquery'], {save: true});
+    if (this.props.jquery !== 'lite') {
+      this.bowerInstall(['jquery=jquery#^' + this.props.jquery], {save: true});
     }
+    this.bowerInstall(['angular=angular#^' + this.props.angular], {save: true});
   }
 });
